246125adb02eaea04f1bf97a57700e08c17e991b
Pat Rocchio <procchio6@gmail.com>
2017-06-29 08:58:21 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+  }
 
+  engageWarpDrive() {
+    if (this.position === 'Pilot' && this.currentShip.constructor === Spaceship) {
+      this.currentShip.warpDrive = 'engaged!'
+    }else {
+      return 'had no effect'
+    }
+  }
+
+  setsInvisibility() {
+    if (this.position === 'Defender' && this.currentShip.constructor === Spaceship) {
+      this.currentShip.cloaked = true
+    }else {
+      return 'had no effect'
+    }
+  }
+
+  chargePhasers() {
+    if (this.position === 'Gunner' && this.currentShip.constructor === Spaceship) {
+      this.currentShip.phasersCharge = 'charged!'
+    } else {
+      return 'had no effect'
+    }
+  }
+}
